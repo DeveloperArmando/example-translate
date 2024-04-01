34e416a985e767f1047f36d5ff6fce2ef56917a3
Armando Santiago <jose.santiago@ryndem.mx>
2024-04-01 12:13:08 -0600

    feat: funcionalidad de traducción con ngx-translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-translate';
+  nombreMio = "Armando";
+  constructor(private translate: TranslateService) {
+    const lang = translate.getBrowserLang();
+    // const lang = translate.getBrowserCultureLang();
+    if (lang) {
+      console.log('Language detected ->', lang);
+      translate.setDefaultLang(lang);
+      translate.use(lang);
+
+      translate.get('demo.greeting', {name: 'John'}).subscribe((res: string) => {
+        console.log(res);
+      });
+    }
+
+    translate.get('demo.greeting', {name: 'John'}).subscribe((res: string) => {
+      console.log(res);
+    });
+
+    console.log(translate.instant('demo.greeting', {name: 'John'}));
+  }
+
+  useLanguage(language: string): void {
+    this.translate.use(language);
+}
+
 }
